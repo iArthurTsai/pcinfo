fac0cc603be0de7cb95f776bf177c0883d7ee638
iArthurTsai <67720373+iArthurTsai@users.noreply.github.com>
2025-03-02 09:04:45 +0800

    Add ANSI Escape Code

---
+++ color.bat
@@ -0,0 +1,283 @@
+@echo off
+echo %date% %time%
+echo.
+echo 0
+echo [30m Black Text [0m
+echo [31m Red Text [0m
+echo [32m Green Text [0m
+echo [33m Yellow Text [0m
+echo [34m Blue Text [0m
+echo [35m Magenta Text [0m
+echo [36m Cyan Text [0m
+echo [37m White Text [0m
+echo [90m Bright Black Text [0m
+echo [91m Bright Red Text [0m
+echo [92m Bright Green Text [0m
+echo [93m Bright Yellow Text [0m
+echo [94m Bright Blue Text [0m
+echo [95m Bright Magenta Text [0m
+echo [96m Bright Cyan Text [0m
+echo [97m Bright White Text [0m
+echo.
+echo 1
+echo [1;30m Black Text [0m
+echo [1;31m Red Text [0m
+echo [1;32m Green Text [0m
+echo [1;33m Yellow Text [0m
+echo [1;34m Blue Text [0m
+echo [1;35m Magenta Text [0m
+echo [1;36m Cyan Text [0m
+echo [1;37m White Text [0m
+echo [1;90m Bright Black Text [0m
+echo [1;91m Bright Red Text [0m
+echo [1;92m Bright Green Text [0m
+echo [1;93m Bright Yellow Text [0m
+echo [1;94m Bright Blue Text [0m
+echo [1;95m Bright Magenta Text [0m
+echo [1;96m Bright Cyan Text [0m
+echo [1;97m Bright White Text [0m
+echo.
+echo 2
+echo [2;30m Black Text [0m
+echo [2;31m Red Text [0m
+echo [2;32m Green Text [0m
+echo [2;33m Yellow Text [0m
+echo [2;34m Blue Text [0m
+echo [2;35m Magenta Text [0m
+echo [2;36m Cyan Text [0m
+echo [2;37m White Text [0m
+echo [2;90m Bright Black Text [0m
+echo [2;91m Bright Red Text [0m
+echo [2;92m Bright Green Text [0m
+echo [2;93m Bright Yellow Text [0m
+echo [2;94m Bright Blue Text [0m
+echo [2;95m Bright Magenta Text [0m
+echo [2;96m Bright Cyan Text [0m
+echo [2;97m Bright White Text [0m
+echo.
+echo 3
+echo [3;30m Black Text [0m
+echo [3;31m Red Text [0m
+echo [3;32m Green Text [0m
+echo [3;33m Yellow Text [0m
+echo [3;34m Blue Text [0m
+echo [3;35m Magenta Text [0m
+echo [3;36m Cyan Text [0m
+echo [3;37m White Text [0m
+echo [3;90m Bright Black Text [0m
+echo [3;91m Bright Red Text [0m
+echo [3;92m Bright Green Text [0m
+echo [3;93m Bright Yellow Text [0m
+echo [3;94m Bright Blue Text [0m
+echo [3;95m Bright Magenta Text [0m
+echo [3;96m Bright Cyan Text [0m
+echo [3;97m Bright White Text [0m
+echo.
+echo 4
+echo [4;30m Black Text [0m
+echo [4;31m Red Text [0m
+echo [4;32m Green Text [0m
+echo [4;33m Yellow Text [0m
+echo [4;34m Blue Text [0m
+echo [4;35m Magenta Text [0m
+echo [4;36m Cyan Text [0m
+echo [4;37m White Text [0m
+echo [4;90m Bright Black Text [0m
+echo [4;91m Bright Red Text [0m
+echo [4;92m Bright Green Text [0m
+echo [4;93m Bright Yellow Text [0m
+echo [4;94m Bright Blue Text [0m
+echo [4;95m Bright Magenta Text [0m
+echo [4;96m Bright Cyan Text [0m
+echo [4;97m Bright White Text [0m
+echo.
+echo 5
+echo [5;30m Black Text [0m
+echo [5;31m Red Text [0m
+echo [5;32m Green Text [0m
+echo [5;33m Yellow Text [0m
+echo [5;34m Blue Text [0m
+echo [5;35m Magenta Text [0m
+echo [5;36m Cyan Text [0m
+echo [5;37m White Text [0m
+echo [5;90m Bright Black Text [0m
+echo [5;91m Bright Red Text [0m
+echo [5;92m Bright Green Text [0m
+echo [5;93m Bright Yellow Text [0m
+echo [5;94m Bright Blue Text [0m
+echo [5;95m Bright Magenta Text [0m
+echo [5;96m Bright Cyan Text [0m
+echo [5;97m Bright White Text [0m
+echo.
+echo 6
+echo [6;30m Black Text [0m
+echo [6;31m Red Text [0m
+echo [6;32m Green Text [0m
+echo [6;33m Yellow Text [0m
+echo [6;34m Blue Text [0m
+echo [6;35m Magenta Text [0m
+echo [6;36m Cyan Text [0m
+echo [6;37m White Text [0m
+echo [6;90m Bright Black Text [0m
+echo [6;91m Bright Red Text [0m
+echo [6;92m Bright Green Text [0m
+echo [6;93m Bright Yellow Text [0m
+echo [6;94m Bright Blue Text [0m
+echo [6;95m Bright Magenta Text [0m
+echo [6;96m Bright Cyan Text [0m
+echo [6;97m Bright White Text [0m
+echo.
+echo 7
+echo [7;30m Black Text [0m
+echo [7;31m Red Text [0m
+echo [7;32m Green Text [0m
+echo [7;33m Yellow Text [0m
+echo [7;34m Blue Text [0m
+echo [7;35m Magenta Text [0m
+echo [7;36m Cyan Text [0m
+echo [7;37m White Text [0m
+echo [7;90m Bright Black Text [0m
+echo [7;91m Bright Red Text [0m
+echo [7;92m Bright Green Text [0m
+echo [7;93m Bright Yellow Text [0m
+echo [7;94m Bright Blue Text [0m
+echo [7;95m Bright Magenta Text [0m
+echo [7;96m Bright Cyan Text [0m
+echo [7;97m Bright White Text [0m
+echo.
+echo 8
+echo [8;30m Black Text [0m
+echo [8;31m Red Text [0m
+echo [8;32m Green Text [0m
+echo [8;33m Yellow Text [0m
+echo [8;34m Blue Text [0m
+echo [8;35m Magenta Text [0m
+echo [8;36m Cyan Text [0m
+echo [8;37m White Text [0m
+echo [8;90m Bright Black Text [0m
+echo [8;91m Bright Red Text [0m
+echo [8;92m Bright Green Text [0m
+echo [8;93m Bright Yellow Text [0m
+echo [8;94m Bright Blue Text [0m
+echo [8;95m Bright Magenta Text [0m
+echo [8;96m Bright Cyan Text [0m
+echo [8;97m Bright White Text [0m
+echo.
+echo 9
+echo [9;30m Black Text [0m
+echo [9;31m Red Text [0m
+echo [9;32m Green Text [0m
+echo [9;33m Yellow Text [0m
+echo [9;34m Blue Text [0m
+echo [9;35m Magenta Text [0m
+echo [9;36m Cyan Text [0m
+echo [9;37m White Text [0m
+echo [9;90m Bright Black Text [0m
+echo [9;91m Bright Red Text [0m
+echo [9;92m Bright Green Text [0m
+echo [9;93m Bright Yellow Text [0m
+echo [9;94m Bright Blue Text [0m
+echo [9;95m Bright Magenta Text [0m
+echo [9;96m Bright Cyan Text [0m
+echo [9;97m Bright White Text [0m
+echo.
+echo 21
+echo [21;30m Black Text [0m
+echo [21;31m Red Text [0m
+echo [21;32m Green Text [0m
+echo [21;33m Yellow Text [0m
+echo [21;34m Blue Text [0m
+echo [21;35m Magenta Text [0m
+echo [21;36m Cyan Text [0m
+echo [21;37m White Text [0m
+echo [21;90m Bright Black Text [0m
+echo [21;91m Bright Red Text [0m
+echo [21;92m Bright Green Text [0m
+echo [21;93m Bright Yellow Text [0m
+echo [21;94m Bright Blue Text [0m
+echo [21;95m Bright Magenta Text [0m
+echo [21;96m Bright Cyan Text [0m
+echo [21;97m Bright White Text [0m
+echo.
+echo 38
+echo [38;30m Black Text [0m
+echo [38;31m Red Text [0m
+echo [38;32m Green Text [0m
+echo [38;33m Yellow Text [0m
+echo [38;34m Blue Text [0m
+echo [38;35m Magenta Text [0m
+echo [38;36m Cyan Text [0m
+echo [38;37m White Text [0m
+echo [38;90m Bright Black Text [0m
+echo [38;91m Bright Red Text [0m
+echo [38;92m Bright Green Text [0m
+echo [38;93m Bright Yellow Text [0m
+echo [38;94m Bright Blue Text [0m
+echo [38;95m Bright Magenta Text [0m
+echo [38;96m Bright Cyan Text [0m
+echo [38;97m Bright White Text [0m
+echo.
+echo 48
+echo [48;30m Black Text [0m
+echo [48;31m Red Text [0m
+echo [48;32m Green Text [0m
+echo [48;33m Yellow Text [0m
+echo [48;34m Blue Text [0m
+echo [48;35m Magenta Text [0m
+echo [48;36m Cyan Text [0m
+echo [48;37m White Text [0m
+echo [48;90m Bright Black Text [0m
+echo [48;91m Bright Red Text [0m
+echo [48;92m Bright Green Text [0m
+echo [48;93m Bright Yellow Text [0m
+echo [48;94m Bright Blue Text [0m
+echo [48;95m Bright Magenta Text [0m
+echo [48;96m Bright Cyan Text [0m
+echo [48;97m Bright White Text [0m
+echo.
+echo 53
+echo [53;30m Black Text [0m
+echo [53;31m Red Text [0m
+echo [53;32m Green Text [0m
+echo [53;33m Yellow Text [0m
+echo [53;34m Blue Text [0m
+echo [53;35m Magenta Text [0m
+echo [53;36m Cyan Text [0m
+echo [53;37m White Text [0m
+echo [53;90m Bright Black Text [0m
+echo [53;91m Bright Red Text [0m
+echo [53;92m Bright Green Text [0m
+echo [53;93m Bright Yellow Text [0m
+echo [53;94m Bright Blue Text [0m
+echo [53;95m Bright Magenta Text [0m
+echo [53;96m Bright Cyan Text [0m
+echo [53;97m Bright White Text [0m
+echo.
+echo Background
+echo [37;40m White Text + Black Background [0m
+echo [37;41m White Text + Red Background [0m
+echo [37;42m White Text + Green Background [0m
+echo [37;43m White Text + Yellow Background [0m
+echo [37;44m White Text + Blue Background [0m
+echo [37;45m White Text + Magenta Background [0m
+echo [37;46m White Text + Cyan Background [0m
+echo [37;47m White Text + White Background [0m
+echo [37;100m White Text + Bright Black Background [0m
+echo [37;101m White Text + Bright Red Background [0m
+echo [37;102m White Text + Bright Green Background [0m
+echo [37;103m White Text + Bright Yellow Background [0m
+echo [37;104m White Text + Bright Blue Background [0m
+echo [37;105m White Text + Bright Magenta Background [0m
+echo [37;106m White Text + Bright Cyan Background [0m
+echo [37;107m White Text + Bright White Background [0m
+echo.
+pause
+
+for /L %%S in (0,1,107) do (
+    echo %%S
+    for %%C in (30 31 32 33 34 35 36 37 90 91 92 93 94 95 96 97) do (
+        echo [%%S;%%Cm Style %%S with Color %%C [0m
+    )
+    echo.
+)
+pause
